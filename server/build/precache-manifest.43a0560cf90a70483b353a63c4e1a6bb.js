0bd7447a72d1da1a81602563f41aba439b4672ce
Oscar Tu <o.car.w2@gmail.com>
2020-05-31 04:15:26 -0700

    Add prod build and express generator to deploy to heroku

---
+++ server/build/precache-manifest.43a0560cf90a70483b353a63c4e1a6bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90f7796dffd541714ca1c4cd29b07686",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7296a8cfbf852c73fd2e",
+    "url": "/static/css/main.f74a2b47.chunk.css"
+  },
+  {
+    "revision": "a3b756544a1c68d346b4",
+    "url": "/static/js/2.87f49b78.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.87f49b78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7296a8cfbf852c73fd2e",
+    "url": "/static/js/main.c1acf644.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
